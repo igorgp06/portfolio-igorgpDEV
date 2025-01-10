5b76f2879c0b4ebcea55666f8b29febc0d326948
igorgp06 <contateme.igorgp@gmail.com>
2025-01-10 01:53:56 -0300

    [WORK 1.1] Progresso da seção header sem a responsividade completa.

---
+++ assets/js/index/header.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.querySelector('.toggle-button');
+    const closeIcon = document.querySelector('.close-icon');
+    const navbarLinks = document.querySelector('.navbar-links');
+    const links = document.querySelectorAll('.links li a');
+
+    function resetMenu() {
+        if (window.innerWidth > 950) {
+            navbarLinks.classList.remove('active');
+            closeIcon.style.display = 'none';
+            toggleButton.style.display = 'none';
+        } else {
+            toggleButton.style.display = 'flex';
+        }
+    };
+
+    toggleButton.addEventListener('click', function () {
+        navbarLinks.classList.add('active');
+        toggleButton.style.display = 'none';
+        closeIcon.style.display = 'block';
+    });
+
+    closeIcon.addEventListener('click', function () {
+        navbarLinks.classList.remove('active');
+        closeIcon.style.display = 'none';
+        toggleButton.style.display = 'flex';
+    });
+
+    links.forEach(link => {
+        link.addEventListener('click', function () {
+            navbarLinks.classList.remove('active');
+            closeIcon.style.display = 'none';
+            if (window.innerWidth <= 950) {
+                toggleButton.style.display = 'flex';
+            } else [
+                toggleButton.style.display = 'none'
+            ]
+        })
+    });
+
+    window.addEventListener('resize', resetMenu);
+
+    resetMenu()
+});
